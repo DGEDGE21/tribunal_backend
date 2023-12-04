15d036473c640bca6733fbea27f04d7f4dd1b243
DGEDGE21 <manoanisio2000@gmail.com>
2023-12-04 12:15:18 +0200

    12/4/202322233

--- tribunal/settings.py
+++ tribunal/settings.py
@@ -98,10 +98,9 @@ CORS_ORIGIN_WHITELIST=[
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
